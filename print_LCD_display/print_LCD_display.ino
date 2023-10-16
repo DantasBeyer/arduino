01b8cab0a9d53bbaf6b789d9d227451fae8a3d04
João Beyer <100128679+DantasBeyer@users.noreply.github.com>
2023-10-16 21:18:36 +0200

    Update print_LCD_display.ino

--- print_LCD_display/print_LCD_display.ino
+++ print_LCD_display/print_LCD_display.ino
@@ -4,9 +4,9 @@ LiquidCrystal lcd(12,11,5,4,3,2);
 void setup()
 {
   lcd.begin(20,2);
-  lcd.print("print");
+  lcd.print("Print on");
   lcd.setCursor(0,1);
-  lcd.print("LCD_Display");
+  lcd.print("LCD Display");
 
 }
 void loop()
