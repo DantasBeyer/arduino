eee2c0e284b1bede81d61233799f495332599bb8
João Beyer <100128679+DantasBeyer@users.noreply.github.com>
2023-10-16 21:16:50 +0200

    Update print_LCD_display.ino

--- print_LCD_display/print_LCD_display.ino
+++ print_LCD_display/print_LCD_display.ino
@@ -4,9 +4,9 @@ LiquidCrystal lcd(12,11,5,4,3,2);
 void setup()
 {
   lcd.begin(20,2);
-  lcd.print("Vai pro");
+  lcd.print("print");
   lcd.setCursor(0,1);
-  lcd.print("CRLALHO");
+  lcd.print("LCD_Display");
 
 }
 void loop()
